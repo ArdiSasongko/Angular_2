f33cbaf013f4f2a7e53d004eec2012e81f4ea2e4
ArdiSasongko <ardisasongko002@gmail.com>
2022-11-25 19:18:54 +0700

    tes

--- app-learning/src/app/app.module.ts
+++ app-learning/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
